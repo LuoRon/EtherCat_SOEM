087e0f835889f6e30ec1131140e16e355f01afe3
LuoRon <58618214+LuoRon@users.noreply.github.com>
2022-05-18 14:31:11 +0800

    Delete scripts directory

--- scripts/data_log.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ../result_data/lcm_log
-#lcm-logger ./data$(date +%F).lcm
-lcm-logger ./data.lcm
--- scripts/lcm-log2smat/pod-build/CMakeFiles/Pn1ZRmONcJ2zpQf05ZEpG5hTm7tubEA7.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Pn1ZRmONcJ2zpQf05ZEpG5hTm7tubEA7.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
